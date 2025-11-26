33d52d242b91a71dff8d2bc0346ee128b918bea6
sang-won-Park05 <conbread01@naver.com>
2025-11-26 15:05:59 +0900

    ㄹㅇ찐찐

--- back/app/main.py
+++ back/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from datetime import datetime
 
-app = FastAPI()
+app = FastAPI(
+    title="나의 멋진 API",
+    description="이것은 내 FastAPI 애플리케이션의 설명입니다.",
+    version="1.0.0"
+)
 
 @app.get("/health", status_code=200)
 def health_check():
